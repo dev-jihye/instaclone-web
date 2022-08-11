f82393a44051a61a33ec9b26ca1c50642882fa0a
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-11 17:40:28 +0900

    ✨ Router 셋팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import NotFound from './screens/NotFound';
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = true;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {isLoggedIn ? (
+            <Route path="/" element={<Home />}></Route>
+          ) : (
+            <Route path="/" element={<Login />}></Route>
+          )}
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
