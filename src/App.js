0291aec8daf2e90ca4a3719e34cb054ff8357c10
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-12 18:44:52 +0900

    ✨ apollo client 셋팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useReactiveVar } from '@apollo/client';
+import { ApolloProvider, useReactiveVar } from '@apollo/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { darkModeVar, isLoggedInVar } from './apollo';
+import { client, darkModeVar, isLoggedInVar } from './apollo';
 import routes from './routes';
 import Home from './screens/Home';
 import Login from './screens/Login';
@@ -14,24 +14,26 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <Router>
-          <Routes>
-            {isLoggedIn ? (
-              <Route path={routes.home} element={<Home />}></Route>
-            ) : (
-              <Route path={routes.home} element={<Login />}></Route>
-            )}
-            {!isLoggedIn ? (
-              <Route path={routes.signUp} element={<SignUp />}></Route>
-            ) : null}
-            <Route path="*" element={<NotFound />}></Route>
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Routes>
+              {isLoggedIn ? (
+                <Route path={routes.home} element={<Home />}></Route>
+              ) : (
+                <Route path={routes.home} element={<Login />}></Route>
+              )}
+              {!isLoggedIn ? (
+                <Route path={routes.signUp} element={<SignUp />}></Route>
+              ) : null}
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
--- src/apollo.js
+++ src/apollo.js
@@ -1,4 +1,10 @@
-import { makeVar } from '@apollo/client';
+import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
 
 export const isLoggedInVar = makeVar(false);
 export const darkModeVar = makeVar(false);
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  // apollo가 가져온 정보를 기억
+  cache: new InMemoryCache(),
+});
