088bd0cf7d126f5c1405d1b293f0ec2692af04f6
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-12 15:31:55 +0900

    🎨 button 수정

--- src/components/auth/Button.js
+++ src/components/auth/Button.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const SButton = styled.input`
+const Button = styled.input`
   border: none;
   margin-top: 12px;
   border-radius: 3px;
@@ -12,6 +12,4 @@ const SButton = styled.input`
   width: 100%;
 `;
 
-export default function Button(props) {
-  return <SButton {...props} />;
-}
+export default Button;
