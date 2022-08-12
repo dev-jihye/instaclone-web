9b5a6d64d162cd2f3bffe92572a7db999f5c5019
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-12 15:32:51 +0900

    ✨ sign up 마크업 및 스타일링

---
+++ src/screens/SignUp.js
@@ -0,0 +1,46 @@
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import AuthLayout from '../components/auth/AuthLayout';
+import Button from '../components/auth/Button';
+import routes from '../routes';
+import Input from '../components/auth/Input';
+import FormBox from '../components/auth/FormBox';
+import BottomBox from '../components/auth/BottomBox';
+import { BoldLink } from '../components/shared';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Subtitle = styled(BoldLink)`
+  font-size: 16px;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+function SignUp() {
+  return (
+    <AuthLayout>
+      <FormBox>
+        <HeaderContainer>
+          <FontAwesomeIcon icon={faInstagram} size="3x" />
+          <Subtitle>
+            Sign up to see photos and videos from your friends.
+          </Subtitle>
+        </HeaderContainer>
+        <form>
+          <Input type="text" placeholder="Name" />
+          <Input type="text" placeholder="Email" />
+          <Input type="text" placeholder="username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" value="Sign up" />
+        </form>
+      </FormBox>
+      <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
+    </AuthLayout>
+  );
+}
+export default SignUp;
