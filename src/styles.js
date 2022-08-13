08527497bd22c49b4f5dfbd417e442a2eeb06e2c
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-14 00:03:55 +0900

    💄 darkMode styling

--- src/styles.js
+++ src/styles.js
@@ -3,27 +3,33 @@ import reset from 'styled-reset';
 
 export const lightTheme = {
   accent: '#0095f6',
+  bgColor: '#FAFAFA',
+  fontColor: 'rgb(38, 38, 38)',
   borderColor: 'rgb(219,219,219)',
 };
 
 export const darkTheme = {
-  fontColor: 'white',
+  accent: '#0095f6',
   bgColor: '#2c2c2c',
+  fontColor: 'white',
+  borderColor: '#fafafa',
+  inputColor: 'rgb(38, 38, 38)',
 };
 
 export const GlobalStyles = createGlobalStyle`
   ${reset}
   input {
     all:unset;
+    color: ${(props) => props.theme.inputColor};
   }
   * {
     box-sizing:border-box;
   }
   body { 
-    background-color: #FAFAFA;
+    background-color: ${(props) => props.theme.bgColor};
     font-size:14px;
     font-family:'Open Sans', sans-serif;
-    color: rgb(38, 38, 38);
+    color: ${(props) => props.theme.fontColor};
   }
   a {
     text-decoration: none;
