80d8b47bffe143f43dd8527cff1d360c2db2eafc
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-15 15:12:25 +0900

    ✨ feed username, avatar 보여주기

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,53 @@
-import { useNavigate } from 'react-router-dom';
-import { logUserOut } from '../apollo';
+import { gql, useQuery } from '@apollo/client';
+import Avatar from '../components/Avatar';
+import styled from 'styled-components';
+import { BoldText } from '../components/shared';
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(BoldText)`
+  margin-left: 5px;
+`;
 
 export default function Home() {
-  const navigate = useNavigate();
+  const { data } = useQuery(FEED_QUERY);
   return (
     <div>
-      <h1>Welcome!</h1>
-      <button onClick={() => logUserOut(navigate)}>Log out now!</button>
+      {data?.seeFeed?.map((photo) => (
+        <PhotoContainer key={photo.id}>
+          <PhotoHeader>
+            <Avatar url={photo.user.avatar} />
+            <Username>{photo.user.username}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
     </div>
   );
 }
