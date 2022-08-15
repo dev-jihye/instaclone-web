c933c13e63402052d8da126cc71f9e623c9746d5
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-15 18:18:42 +0900

    ✨ Photo 좋아요 수, 좋아요 여부 구현

---
+++ src/components/feed/Photo.js
@@ -0,0 +1,155 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from '@fortawesome/free-regular-svg-icons';
+import { faHeart as SolidHeart } from '@fortawesome/free-solid-svg-icons';
+import Proptypes from 'prop-types';
+import { BoldText } from '../shared';
+import Avatar from '../Avatar';
+import { gql, useMutation } from '@apollo/client';
+
+const TOGGLE_LIKE_MUTATION = gql`
+  mutation toggleLike($id: Int!) {
+    toggleLike(id: $id) {
+      ok
+      error
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin: 0 auto;
+  margin-bottom: 20px;
+  max-width: 615px;
+  border-radius: 4px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const Username = styled(BoldText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 12px 15px;
+`;
+
+const PhotoActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const PhotoAction = styled.div`
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const Likes = styled(BoldText)`
+  margin-top: 15px;
+  display: block;
+`;
+
+export default function Photo({ id, user, file, isLiked, likes }) {
+  // result = 백엔드로 부터 받은 데이터, cache = cache를 제어할 수 있는 링크
+  const updateToggleLike = (cache, result) => {
+    const {
+      data: {
+        toggleLike: { ok },
+      },
+    } = result;
+    if (ok) {
+      const fragmentId = `Photo:${id}`;
+      const fragment = gql`
+        fragment BSName on Photo {
+          isLiked
+          likes
+        }
+      `;
+      // cache에서 fragmentId에 해당하는 데이터를 가져옴
+      const result = cache.readFragment({
+        id: fragmentId,
+        fragment,
+      });
+      if ('isLiked' in result && 'likes' in result) {
+        const { isLiked: cacheIsLiked, likes: cacheLikes } = result;
+        cache.writeFragment({
+          id: fragmentId,
+          fragment: fragment,
+          data: {
+            isLiked: !cacheIsLiked,
+            likes: cacheIsLiked ? cacheLikes - 1 : cacheLikes + 1,
+          },
+        });
+      }
+    }
+  };
+  const [toggleLikeMutation, { loading }] = useMutation(TOGGLE_LIKE_MUTATION, {
+    variables: {
+      id,
+    },
+    update: updateToggleLike,
+  });
+  return (
+    <PhotoContainer key={id}>
+      <PhotoHeader>
+        <Avatar lg url={user.avatar} />
+        <Username>{user.username}</Username>
+      </PhotoHeader>
+      <PhotoFile src={file}></PhotoFile>
+      <PhotoData>
+        <PhotoActions>
+          <div>
+            <PhotoAction onClick={toggleLikeMutation}>
+              <FontAwesomeIcon
+                style={{ color: isLiked ? 'tomato' : 'inherit' }}
+                icon={isLiked ? SolidHeart : faHeart}
+              />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faComment} />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </PhotoAction>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+        </PhotoActions>
+        <Likes>{likes === 1 ? '1 like' : `${likes} likes`}</Likes>
+      </PhotoData>
+    </PhotoContainer>
+  );
+}
+
+Photo.propTypes = {
+  id: Proptypes.number.isRequired,
+  user: Proptypes.shape({
+    avatar: Proptypes.string,
+    username: Proptypes.string.isRequired,
+  }),
+  file: Proptypes.string.isRequired,
+  isLiked: Proptypes.bool.isRequired,
+  likes: Proptypes.number.isRequired,
+};
