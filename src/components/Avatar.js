c39fe8ffcd979a7da993b1d0e1dcff3376e13344
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-15 14:35:00 +0900

    ✨ avatar 컴포넌트 추가

---
+++ src/components/Avatar.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const SAvatar = styled.div`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background-color: #2c2c2c;
+  overflow: hidden;
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+`;
+
+export default function Avatar({ url = '' }) {
+  return (
+    <SAvatar>{url !== '' ? <Img src={url} alt="avatar" /> : null}</SAvatar>
+  );
+}
