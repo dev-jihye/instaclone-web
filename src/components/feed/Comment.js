3fbced3429f859a52572ce6b65aa3b68405928ec
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-16 00:37:00 +0900

    ✨ 해시태그 파싱하여 스타일링 추가

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,18 +1,36 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { BoldText } from '../shared';
+import sanitizeHtml from 'sanitize-html';
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 15px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 export default function Comment({ author, payload }) {
+  const cleanedPayload = sanitizeHtml(
+    payload?.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, '<mark>$&</mark>'),
+    { allowedTags: ['mark'] }
+  );
+
   return (
     <CommentContainer>
       <BoldText>{author}</BoldText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      ></CommentCaption>
     </CommentContainer>
   );
 }
