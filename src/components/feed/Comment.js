454f6eacd623768f2fc3fc225e59aa258382a3a6
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-08-16 14:51:46 +0900

    ✨ comment 제거

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -3,8 +3,19 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { BoldText } from '../shared';
 import { Link } from 'react-router-dom';
+import { gql, useMutation } from '@apollo/client';
 
-const CommentContainer = styled.div``;
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($id: Int!) {
+    deleteComment(id: $id) {
+      ok
+    }
+  }
+`;
+
+const CommentContainer = styled.div`
+  margin-bottom: 7px;
+`;
 
 const CommentCaption = styled.span`
   margin-left: 15px;
@@ -18,7 +29,34 @@ const CommentCaption = styled.span`
   }
 `;
 
-export default function Comment({ author, payload }) {
+export default function Comment({ id, photoId, isMine, author, payload }) {
+  const updateDeleteComment = (cache, result) => {
+    const {
+      data: {
+        deleteComment: { ok },
+      },
+    } = result;
+    if (ok) {
+      cache.evict({ id: `Comment:${id}` });
+      cache.modify({
+        id: `Photo:${photoId}`,
+        fields: {
+          commentNumber(prev) {
+            return prev - 1;
+          },
+        },
+      });
+    }
+  };
+  const [deleteComment] = useMutation(DELETE_COMMENT_MUTATION, {
+    variables: {
+      id,
+    },
+    update: updateDeleteComment,
+  });
+  const onDeleteClick = () => {
+    deleteComment();
+  };
   return (
     <CommentContainer>
       <BoldText>{author}</BoldText>
@@ -33,11 +71,15 @@ export default function Comment({ author, payload }) {
           )
         )}
       </CommentCaption>
+      {isMine ? <button onClick={onDeleteClick}>X</button> : null}
     </CommentContainer>
   );
 }
 
 Comment.protoTypes = {
+  id: PropTypes.number,
   author: PropTypes.string.isRequired,
   payload: PropTypes.string.isRequired,
+  isMine: PropTypes.bool,
+  photoId: PropTypes.number,
 };
